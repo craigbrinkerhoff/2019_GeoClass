9c8a60660628f1a0cb2c55187888d3d69f9e34c9
Craig Brinkerhoff <cbrinkerhoff@umass.edu>
2019-10-16 15:26:27 -0400

    Add files via upload

--- scripts/switch_test.R
+++ scripts/switch_test.R
@@ -118,32 +118,36 @@ batchbam = function(file,directory,errorflag, minxs, mintime){
   bamdata=bamr::bam_data(w=W_obs,dA=dA,s=S_obs,Qhat=as.vector(Q_prior))
   
   #functions calculated in jupyter notebook and inputted manually here
-  bhat_func <- function(x){0.0569+0.8801* sd(log10(x))} #r2 0.726
-  a0Hat_func <- function(x){-0.1267+-0.1887* sd(log10(x))+1.6930*mean(log10(x))} #r2 0.907
-  Wbhat_func <- function(x){0.0016+1.0028*mean(log10(x))} #r2 0.984
-  Dbhat_func <- function(x){-1.1374-0.2436* sd(log10(x))+0.6854*mean(log10(x))} #r2 0.640
-  rhat_func <- function(x){0.6185-1.9097* sd(log10(x))+0.0420*mean(log10(x))} #r2 0.421
+  bhat_func <- function(x){0.0569+0.3822* sd(log(x))} #r2 0.726
+  a0Hat_func <- function(x){-0.2918-0.1887* sd(log(x))+1.6930*mean(log(x))} #r2 0.907
+  Wbhat_func <- function(x){0.0037+1.0028*mean(log(x))} #r2 0.984
+  Dbhat_func <- function(x){-2.6189-0.2436* sd(log(x))+0.6854*mean(log(x))} #r2 0.640
+  rhat_func <- function(x){1.4241-1.9097* sd(log(x))+0.0420*mean(log(x))} #r2 0.421
+
+  nhat_func <- function(x) {-0.1636+0.4077*log(mean(x))} #r2: 0.631 for entire river
+  nhat_func_new <- function(x) {-0.1636+0.4077*mean(log(x))} #r2: 0.631
+  #bhat_func_old <- function(x) {0.02161+0.4578*sd(log(x))} #mark's b model
+  
+  #note this is log10
   Qbhat_func <- function(x) {-1.8699 + 1.8871*mean(log10(x))} #r2 0.89
-  nhat_func <- function(x) {-0.0892+0.4037*log10(mean(x))} #r2: 0.636
-  bhat_func_old <- function(x) {0.02161+0.4578*sd(log10(x))} #mark's b model
   
   #hat variables
   a0hat <- apply(W_obs, 1, a0Hat_func)
   Wbhat <- apply(W_obs, 1, Wbhat_func)
   Dbhat <- apply(W_obs, 1, Dbhat_func)
   Qbhat <- apply(W_obs, 1, Qbhat_func)
-  nhat <- nhat_func(S_obs)
-  bhat <- apply(W_obs, 1, bhat_func_old)
+  nhat <- nhat_func_new(S_obs)
+  bhat <- apply(W_obs, 1, bhat_func)
   rhat <- apply(W_obs, 1, rhat_func)
   
-  #AHG/AMHG Flag
+  #AHG/AMHG Flag (in log base 10 NOT natural log like everyting else)
   H_obs = rowMeans(ncvar_get(data_in, 'XS_Timeseries/H'))
   xs_bnds = ncvar_get(data_in, 'XS_Timeseries/X')
   S_obs_model <- c(0,1)
   for (i in 1:length(H_obs)) {
     S_obs_model[i] = abs((H_obs[i]-H_obs[i+1])/(xs_bnds[i+1]-xs_bnds[i]))
   }
-  Dehat <- 19*10^(Dbhat)*S_obs_model
+  Dehat <- 19*exp(Dbhat)*S_obs_model
   
   amhgFlag = 0
   regimeS <- 0.44*Dehat^(1.15)*Qbhat^(-0.46)
@@ -151,35 +155,28 @@ batchbam = function(file,directory,errorflag, minxs, mintime){
   if (regimeFit > 0.90) {amhgFlag = 1}
   else {amhgFlag = 0}
   
-  #priors = bam_priors(bamdata)
-  #assign priors
-  #priors <- bam_priors(bamdata = bamdata, logQ_sd = cv2sigma(1), 
-  #                     b_hat=bhat, logA0_hat = a0hat, logn_hat = nhat,
-  #                    lowerbound_A0 = 10^A0[3,2], upperbound_A0 = 10^A0[7,2],
-  #                    lowerbound_b = 0, upperbound_b = B[7,2],
-  #                    lowerbound_logn = N[3,2], upperbound_logn=N[7,2],
-  #                     b_sd=B[2,2], logA0_sd=A0[2,2], logn_sd=N[2,2])
-  
   upperR <- r[7,2]
   lowerR <- r[3,2]
+  r_sd <- SDs[3,2]
   
   #braided river flag, using values from distribution of all x-sections where r < 1
   if (file == 'Tanana.nc' | file == 'Jamuna.nc' | file == 'Platte.nc'){
     upperR <- 0
-    lowerR <- -10
-    rhat <- rep(-0.1082, nrow(W_obs))
+    lowerR <- -2.58
+    rhat <- rep(-0.249, nrow(W_obs))
+    r_sd <- 0.412
     bhat <- rep(0.405, nrow(W_obs))
   }
   
   priors <- bam_priors(bamdata = bamdata, logQ_sd = cv2sigma(1), 
-                       logr_hat = rhat, logWb_hat = Wbhat, logDb_hat = Dbhat, b_hat = bhat,#logn_hat = nhat, b_hat=bhat, logA0_hat = a0hat, 
-                       #lowerbound_A0 = 10^A0[3,2], upperbound_A0 = 10^A0[7,2],
+                       logr_hat = rhat, logWb_hat = Wbhat, logDb_hat = Dbhat, b_hat = bhat,logn_hat = nhat, logA0_hat = a0hat, 
+                       lowerbound_A0 = exp(A0[3,2]), upperbound_A0 = exp(A0[7,2]),
                        lowerbound_logr = lowerR, upperbound_logr=upperR,
                        lowerbound_logWb = Wb[3,2], upperbound_logWb=Wb[7,2],
                        lowerbound_logDb = Db[3,2], upperbound_logDb=Db[7,2],
-                      # lowerbound_b = B[3,2], upperbound_b = B[7,2],
-                      # lowerbound_logn = N[3,2], upperbound_logn=N[7,2],
-                      logr_sd=SDs[3,2], logWb_sd = SDs[1,2], logDb_sd=SDs[2,2])#,  b_sd=SDs[6,2], logA0_sd=SDs[5,2], logn_sd=SDs[4,2])
+                       lowerbound_b = B[3,2], upperbound_b = B[7,2],
+                       lowerbound_logn = log(0.01), upperbound_logn=log(0.05), #set manually by craig
+                       logr_sd=r_sd, logWb_sd = SDs[1,2], logDb_sd=SDs[2,2], logn_sd=SDs[4,2],  b_sd=SDs[6,2], logA0_sd=SDs[5,2])
   
   
   #custom priors post Hagemann et al 2017
@@ -285,7 +282,7 @@ mintime=3
 
 for (phase in allpepsi2){
   setwd(phase)
-  output_directory=paste('C://Users//cbrinkerhoff//Box Sync//Ongoing Projects//geomorph_class//outputs//new_physics_old_priors_correctSD//')
+  output_directory=paste('C://Users//cbrinkerhoff//Box Sync//Ongoing Projects//geomorph_class//outputs//new_physics_new_priors_correctN_correctSD//')
   phase_files= list.files(pattern = "\\.nc$")
   # phase_files=phase_files[1]
   #output_string= paste(training1,'metricscv2.csv',sep="")
@@ -321,10 +318,8 @@ test_priors <- options_manager(paramnames=c("lowerbound_logQ", "upperbound_logQ"
                                             "lowerbound_b", "upperbound_b", "lowerbound_logWb", 'upperbound_logWb', 'lowerbound_logDb',
                                             'upperbound_logDb', 'lowerbound_logr', 'upperbound_logr',
                                             "sigma_man", "sigma_amhg",
-                                            "logQc_hat", "logWc_hat", "b_hat", "logA0_hat", "logn_hat",  'logWb_hat',
-                                            'logDb_hat', 'logr_hat', 'loga_hat',
-                                            'logQ_sd', "logQc_sd", "logWc_sd", "b_sd",  "logA0_sd", "logn_sd", 'logWb_sd', 'logDb_sd',
-                                            'logr_sd', 'loga_sd',
+                                            "logQc_hat", "logWc_hat", "b_hat", "logA0_hat", "logn_hat",  'logWb_hat', 'logDb_hat', 'logr_hat',
+                                            'logQ_sd', "logQc_sd", "logWc_sd", "b_sd",  "logA0_sd", "logn_sd", 'logWb_sd', 'logDb_sd', 'logr_sd',
                                             "Werr_sd", "Serr_sd", "dAerr_sd"),
                                lowerbound_logQ=bam_settings('lowerbound_logQ'), upperbound_logQ=bam_settings('upperbound_logQ'),
                                lowerbound_A0 = 30, upperbound_A0 = 1000000,
@@ -335,13 +330,12 @@ test_priors <- options_manager(paramnames=c("lowerbound_logQ", "upperbound_logQ"
                                lowerbound_logWb = 0, upperbound_logWb = 0,
                                lowerbound_logDb = 0, upperbound_logDb = 0,
                                lowerbound_logr = 0, upperbound_logr = 0,
-                               lowerbound_loga = 0, upperbound_loga = 0,
                                sigma_man=0.25, sigma_amhg = 0.22,
                                logQc_hat=bam_settings('logQc_hat'), logWc_hat=bam_settings('logWc_hat'),
-                               b_hat=bam_settings('b_hat'), logA0_hat = bam_settings('logA0_hat'), logn_hat = -3.5,
-                               logWb_hat = 0, logDb_hat = 0, logr_hat = 0, loga_hat = 0,
+                               b_hat=bam_settings('b_hat'), logA0_hat = bam_settings('logA0_hat'), logn_hat = -3.5, #W_obs is a placeholder for n
+                               logWb_hat = 0, logDb_hat = 0, logr_hat = 0,
                                logQ_sd=0, logQc_sd = 0.8325546, logWc_sd=4.712493, b_sd=0.05, logA0_sd=0.5,
-                               logn_sd=0.25, logWb_sd = 0, logDb_sd = 0, logr_sd = 0, loga_sd = 0,
+                               logn_sd=0.25, logWb_sd = 0, logDb_sd = 0, logr_sd = 0,
                                Werr_sd=10, Serr_sd=0.00001, dAerr_sd=10)
 
 for (i in 1:(length(phase_files))) {
